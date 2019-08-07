1d4d17e95afde059a321bb98cab1ddc1719bcb51
babylco0 <604666459@qq.com>
2019-08-07 12:10:20 +0800

    修改kivy环境变量值

---
+++ kivy_environment.py
@@ -0,0 +1,15 @@
+import os
+os.environ['KIVY_TEXT'] = 'sdl2'
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class FirstApp(App):
+    """第一个kivy程序"""
+    def build(self):
+        return Button(text='Hello kivy')
+
+
+if __name__ == '__main__':
+    FirstApp().run()
