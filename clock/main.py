f19ab3371eac2422f35ebfb06febcd1ae76d21f1
babylco0 <604666459@qq.com>
2019-08-09 15:37:48 +0800

    格式化时间显示

--- clock/main.py
+++ clock/main.py
@@ -6,6 +6,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 import time
 
+__version__ = "1.0.0"
 
 Builder.load_string("""
 <DigitalClock>:
@@ -17,6 +18,8 @@ Builder.load_string("""
         text_size: self.size
         halign: 'center'
         valign: 'middle'
+        # 设置为markup text
+        markup: True
 """)
 
 
@@ -32,7 +35,16 @@ class DigitalClock(GridLayout):
 
     def update_time(self, dt):
         """更新时间"""
-        self._time.text = time.strftime("%Y-%m-%d\n%H:%M:%S", time.localtime())
+        s = time.localtime().tm_sec  # 秒
+        m = time.localtime().tm_min  # 分
+        h = time.localtime().tm_hour  # 时
+        #  分钟秒对应到rgb
+        r = int(s * 255 / 60)
+        g = int(m * 255 / 60)
+        b = int(h * 255 / 24)
+        c = '{:0>2x}{:0>2x}{:0>2x}'.format(r, g, b)  # 合并rgb值
+        format_time = "[b][i][u]%Y-%m-%d[/u][/i]\n\n[color={}]%H:%M:%S[/color][/b]".format(c)  # markup时间字符串
+        self._time.text = time.strftime(format_time, time.localtime())
 
 
 class ClockApp(App):
